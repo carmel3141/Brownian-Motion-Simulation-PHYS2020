818ccfae5f5bf73132e752689bb392c84bc3b61e
carmel3141 <143565967+carmel3141@users.noreply.github.com>
2026-04-29 10:35:03 +1000

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,21 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n=1000
-moves = np.array([[0,1],[0,-1],[1,0],[-1,0]])
 
-random_walk = np.random.choice(4, size=n)
-steps = moves[random_walk]
+for i in range(5):
+    n=1000
+    moves = np.array([[0,1],[0,-1],[1,0],[-1,0]])
 
-
-position = np.cumsum(steps,axis=0)
-x, y = position[:, 0], position[:, 1]
+    random_walk = np.random.choice(4, size=n)
+    steps = moves[random_walk]
 
 
-plt.plot(position[:,0], position[:,1])
-plt.scatter(0, 0, color='red', label='Start')
-plt.scatter(x[-1],y[-1],color='black', label='End')
-plt.show()
+    position = np.cumsum(steps,axis=0)
+    x, y = position[:, 0], position[:, 1]
+
+    plt.plot(position[:,0], position[:,1])
+    plt.scatter(0, 0, color='red', label='Start')
+    plt.scatter(x[-1],y[-1],color='black', label='End')
+    plt.title(f"2D Random Walk ({n} Steps)")
+    plt.legend()
+    plt.show()
 
 ## need to figure out how to make it do it live and so i know where it is at each step
 
